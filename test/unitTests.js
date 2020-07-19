44d2f2f7b99d403428864ee410013588c8ec9dbc
Grunet <sadikshahidain@gmail.com>
2020-07-19 10:27:33 -0500

    Adding in monitoring of the test inputs/outputs

--- test/unitTests.js
+++ test/unitTests.js
@@ -1,8 +1,29 @@
-var assert = require("assert");
+const assert = require("assert");
+const sinon = require("sinon");
+
 describe("Array", function () {
   describe("#indexOf()", function () {
+    let callIndex = 0;
+
+    before(function () {
+      sinon.spy(Array.prototype, "indexOf");
+    });
+
     it("should return -1 when the value is not present", function () {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+      assert.equal([3, 2, 1].indexOf(4), -1);
+    });
+
+    it("should return 2 when the value is the 3rd element", function () {
+      assert.equal([3, 2, 1].indexOf(1), 2);
+    });
+
+    afterEach(function () {
+      const spyCall = Array.prototype.indexOf.getCall(callIndex);
+      callIndex++;
+
+      console.log(spyCall.thisValue);
+      console.log(spyCall.args);
+      console.log(spyCall.returnValue);
     });
   });
 });
